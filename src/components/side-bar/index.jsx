43e5ee1406fd1810d99dd45625f2f0bd8688731b
Klevin Bazaiti <klevin.bazaiti@oliasoft.com>
2022-12-09 17:56:12 +0100

    create the sidebar

---
+++ src/components/side-bar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SideBarToggle } from './side-bar-toggle';
+
+const SideBar = () => {
+  return (
+    <>
+      <SideBarToggle />
+    </>
+  );
+};
+
+export { SideBar };
