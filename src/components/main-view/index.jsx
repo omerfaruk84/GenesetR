55b34a92efc477fb6fddc2ed790a589621c91c84
Klevin Bazaiti <klevin.bazaiti@oliasoft.com>
2022-12-11 14:38:57 +0100

    use react router dom, on the main view component

--- src/components/main-view/index.jsx
+++ src/components/main-view/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { ROUTES } from '../../common/routes';
 import { HeatMap } from '../heat-map';
 import { TopBar } from '../top-bar';
 import ExampleJson from '../heat-map/example.json';
 import styles from './main-view.module.scss';
+import { NotFoundPage } from '../not-found-page';
 
 const MainView = () => {
   return (
     <div className={styles.mainView}>
-      <TopBar />
-      <HeatMap data={ExampleJson} />
+      <Router>
+        <TopBar />
+        <Routes>
+          <Route exact path={ROUTES.HEATMAP} element={<HeatMap data={ExampleJson} />}/>
+          <Route path="*" element={<NotFoundPage />}/>
+        </Routes>
+      </Router>
     </div>
   );
 };
