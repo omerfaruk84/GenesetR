905812312e3c373e9d1973763f69117c51a66bf4
Klevin Bazaiti <klevin.bazaiti@oliasoft.com>
2022-12-11 14:38:06 +0100

    create the main routes

---
+++ src/common/routes.js
@@ -0,0 +1,27 @@
+const ROUTES = Object.freeze({
+  HOME: '/home',
+  GENEREGULATION: '/generegulation',
+  PCA: '/pca',
+  MDE: '/mde',
+  UMAP: '/umap',
+  TSNE: '/tsne',
+  BI_CLUSTERING: '/bi-clustering',
+  GENE_REGULATION: '/gene-regulation',
+  HEATMAP: '/heatmap',
+});
+
+/**
+ * This function will check the current path (url) and the navigation elements path
+ * if they are equal then thats the active tab in the top bar, otherwise will return false
+ * @param {string} currentPath Url current path
+ * @param {string} navPath Nav element path
+ * @returns Boolean
+ */
+export const isActiveTab = (currentPath, navPath) => {
+  if (currentPath === navPath) {
+    return true;
+  }
+  return false;
+}
+
+export { ROUTES };
