965caf598b72e195ae7e0837f7ea8c1f0d88c52a
omerfaruk84 <omer.faruk.kuzu@gmail.com>
2022-12-12 21:13:25 +0100

    Here it is

--- src/components/not-found-page/index.jsx
+++ src/components/not-found-page/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../common/routes';
 import styles from './not-found-page.module.scss';
-
+import { Bokeh } from '../bokeh';
 const NotFoundPage = () => {
   return (
     <div className={styles.mainView}>
@@ -14,7 +14,9 @@ const NotFoundPage = () => {
           <h3 className="h2">Look like you're lost</h3>
           <p>the page you are looking for not avaible!</p>
           <Link to={ROUTES.HOME} className={styles.link_404}>Go to Home</Link>
+          <Bokeh />
         </div>
+
       </div>
     </div>
   );
