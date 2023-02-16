2f8177d8fc0df8cd6716147e48c0b8eab72533c4
klevinb <oneklevin@gmail.com>
2023-02-16 02:44:12 +0100

    create correlation information page

--- src/pages/correlation/correlation-page.jsx
+++ src/pages/correlation/correlation-page.jsx
@@ -1,38 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Heading, Flex } from "@oliasoft-open-source/react-ui-library";
+import { Heading } from "@oliasoft-open-source/react-ui-library";
 import { HeatMap } from '../../components/heat-map';
 import styles from './correlation-page.module.scss';
 import { ModulePathNames } from '../../store/results/enums';
 
 const CorrelationPage = ({ path, corrResults }) => {
   return (
-    <Flex>
+    <div className={styles.mainView}>
       <Heading>Correlation</Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-        quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-        arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-        Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras
-        dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
-        tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
-        enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-        Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
-        imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
-        ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus
-        eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing
-        sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,
-        hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec
-        vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit
-        amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris
-        sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget
-        bibendum sodales, augue velit cursus nunc,
-      </p>
-      <HeatMap graphData={corrResults} />
-    </Flex>
+      {corrResults ? (
+        <HeatMap graphData={corrResults} />
+      ) : (
+        <>
+          <div className={styles.infoSection}>
+            <p className={styles.moduleTextInfos}>
+              Correlation is a measure of the linear relationship between quantitative variables.
+              Two variables are computed against each other and return the correlation coefficient "r"
+              that indicates the strength and direction of association in a range from -1 to 1.
+            </p>
+            <img alt='correlation' src='/images/correlation.png' />
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
