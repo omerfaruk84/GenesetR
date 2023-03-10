7661e92c4bf30ffcf2c8644761207703240cc3d3
klevinb <oneklevin@gmail.com>
2023-03-10 20:58:08 +0100

    fix styling for new gene list textarea

---
+++ src/components/GeneSelectionBox/GeneSymbolValidatorMessage.jsx
@@ -0,0 +1,218 @@
+import * as React from "react";
+//import styles from './styles.modules. ';
+import styles from "./styles.module.scss";
+
+import {
+  FaTimesCircle,
+  FaQuestionCircle,
+  FaExclamationCircle,
+  FaCheckCircle,
+} from "react-icons/fa";
+//import { GeneValidationResult } from './GeneSymbolValidator';
+//import { OQL } from './OQLTextArea';
+
+import classNames from "classnames";
+import { groupBy, reduce } from "lodash";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+
+const RenderSuggestion = function (props) {
+  console.log("props", props);
+  if (props.genes.length == 0) {
+    let title =
+      "Could not find gene symbol. Click to remove it from the gene list.";
+    let onClick = () => props.replaceGene(props.alias, "");
+    return (
+      <div className={styles.suggestionBubble} title={title} onClick={onClick}>
+        <FaTimesCircle className={styles.icon} />
+        <span className={styles.noChoiceLabel}>{props.alias}</span>
+      </div>
+    );
+  }
+
+  if (props.genes.length == 1) {
+    let { hugoGeneSymbol } = props.genes[0];
+    let title = `'${props.alias}' is a synonym for '${hugoGeneSymbol}'. Click here to replace it with the official symbol.`;
+    let onClick = () => props.replaceGene(props.alias, hugoGeneSymbol);
+    return (
+      <div className={styles.suggestionBubble} title={title} onClick={onClick}>
+        <FaQuestionCircle className={styles.icon} />
+        <span className={styles.singleChoiceLabel}>{props.alias}</span>
+        <span>{`: ${hugoGeneSymbol}`}</span>
+      </div>
+    );
+  }
+
+  let title =
+    "Ambiguous gene symbol. Click on one of the alternatives to replace it.";
+  let options = props.genes.map((gene) => ({
+    label: gene.hugoGeneSymbol,
+    value: gene.hugoGeneSymbol,
+  }));
+  return (
+    <div className={styles.suggestionBubble} title={title}>
+      <FaQuestionCircle className={styles.icon} />
+      <span className={styles.multiChoiceLabel}>{props.alias}</span>
+      <span>{":"}&nbsp;</span>
+      <DropdownButton
+        variant={title.toLowerCase()}
+        size="sm"
+        title="Select symbol"
+        id={`geneReplace_${props.alias}`}
+      >
+        {options.map((item, i) => {
+          return (
+            <Dropdown.Item
+              onClick={() => {
+                props.replaceGene(props.alias, item.value);
+              }}
+              eventKey={i + 1}
+            >
+              {item.label}
+            </Dropdown.Item>
+          );
+        })}
+      </DropdownButton>
+    </div>
+  );
+};
+
+const GeneSymbolValidatorMessageChild = (props) => {
+  if (props.oql instanceof Error) {
+    return (
+      <div className={styles.GeneSymbolValidator}>
+        <span className={styles.errorMessage}>
+          {`Cannot validate gene symbols because of invalid OQL.`}
+          &nbsp;&nbsp;
+          <a
+            className={"underline"}
+            onMouseDown={(e) => {
+              e.preventDefault();
+              props.highlightError();
+            }}
+          >
+            Click to highlight error
+          </a>
+        </span>
+      </div>
+    );
+  }
+
+  if (props.oql.query.length === 0) {
+    return null;
+  }
+
+  if (!props.errorMessageOnly && props.validatingGenes) {
+    return (
+      <div className={styles.GeneSymbolValidator}>
+        <span className={styles.pendingMessage}>
+          Validating gene symbols...
+        </span>
+      </div>
+    );
+  }
+
+  if (props.genes instanceof Error) {
+    return (
+      <div className={styles.GeneSymbolValidator}>
+        <span className={styles.pendingMessage}>
+          Unable to validate gene symbols.
+        </span>
+      </div>
+    );
+  }
+
+  if (props.genes.suggestions.length > 0) {
+    return (
+      <div className={styles.GeneSymbolValidator}>
+        <div
+          className={styles.invalidBubble}
+          title="Please edit the gene symbols."
+        >
+          <FaExclamationCircle className={styles.icon} />
+          <span>Invalid gene symbols.</span>
+        </div>
+
+        {props.genes.suggestions.map((suggestion, index) => (
+          <RenderSuggestion
+            key={index}
+            genes={suggestion.genes}
+            alias={suggestion.alias}
+            replaceGene={props.replaceGene}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  // TDOD: remove this condition once multiple entrez gene ids is supported
+  const hugoGeneSymbolSet = groupBy(
+    props.genes.found,
+    (gene) => gene.hugoGeneSymbol
+  );
+  const genesWithMultipleEntrezGeneIds = reduce(
+    hugoGeneSymbolSet,
+    (acc, genes, hugoGeneSymbol) => {
+      if (genes.length > 1) {
+        acc.push(hugoGeneSymbol);
+      }
+      return acc;
+    },
+    []
+  );
+
+  if (genesWithMultipleEntrezGeneIds.length > 0) {
+    return (
+      <div className={styles.GeneSymbolValidator}>
+        <div
+          className={styles.invalidBubble}
+          title="Please edit the gene symbols."
+        >
+          <FaExclamationCircle className={styles.icon} />
+          <span>
+            The portal does not currently support the following gene(s):
+          </span>
+        </div>
+
+        {genesWithMultipleEntrezGeneIds.map((gene, index) => (
+          <RenderSuggestion
+            key={index}
+            genes={[]}
+            alias={gene}
+            replaceGene={props.replaceGene}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  if (props.errorMessageOnly) {
+    return null;
+  }
+  return (
+    <div
+      className={classNames(styles.GeneSymbolValidator, {
+        [styles.nowrap]: !props.wrapTheContent,
+      })}
+    >
+      <div className={styles.validBubble} title="You can now submit the list.">
+        <FaCheckCircle className={styles.icon} name="check-circle" />
+        <span>All gene symbols are valid.</span>
+      </div>
+    </div>
+  );
+};
+
+export default class GeneSymbolValidatorMessage extends React.Component {
+  render() {
+    if (this.props.children) {
+      return <div id="wideGeneBoxValidationStatus">{this.props.children}</div>;
+    }
+
+    return (
+      <div id="geneBoxValidationStatus">
+        <GeneSymbolValidatorMessageChild {...this.props} />
+      </div>
+    );
+  }
+}
--- src/components/genelist/index.jsx
+++ src/components/genelist/index.jsx
@@ -1,220 +1,232 @@
-import React from 'react';
+import React from "react";
 //import { connect } from 'react-redux';
-import { Heading,Card,Field,toast, Flex, Text, Popover, Input, InputGroup, Spacer, Select, TextArea, Button, Modal, Row,  } from "@oliasoft-open-source/react-ui-library";
-import styles from './main-view.module.scss';
-import { FaTrash, FaSave, FaWindowClose} from 'react-icons/fa';
+import {
+  Heading,
+  Card,
+  Field,
+  toast,
+  Flex,
+  Text,
+  Popover,
+  Input,
+  InputGroup,
+  Spacer,
+  Select,
+  TextArea,
+  Button,
+  Modal,
+  Row,
+} from "@oliasoft-open-source/react-ui-library";
+import styles from "./main-view.module.scss";
+import { FaTrash, FaSave, FaWindowClose } from "react-icons/fa";
 import { useState, useEffect } from "react";
-import { get, set, del, update } from 'idb-keyval';
-import  GeneSymbolValidatorMessage, {GeneSymbolValidatorMessageProps} from '../GeneSelectionBox/GeneSymbolValidatorMessage.tsx';
-
-
+import { get, set, del, update } from "idb-keyval";
+import GeneSymbolValidatorMessage, {
+  GeneSymbolValidatorMessageProps,
+} from "../GeneSelectionBox/GeneSymbolValidatorMessage";
 
 let isLoaded = false;
 var db;
-const dbPromise = window.indexedDB.open('GeneListDB', 1);
+const dbPromise = window.indexedDB.open("GeneListDB", 1);
 
-const Genelist = ({setPerturbationList}) => {
-    //Connect to indexdb and get genelists
+const Genelist = ({ setPerturbationList }) => {
+  //Connect to indexdb and get genelists
 
-    const [currentGenes, setGenes] = useState(""); //sets the current genes in textarea
-    const [currentGeneLists, setGeneLists] = useState([]); //sets the current gene lists in select box
-    const [selectedGeneList, setSelectedGeneList] = useState();//sets the currently selected gene list in the select box
-    const [newGeneListName, setNewGeneListName] = useState();
+  const [currentGenes, setGenes] = useState(""); //sets the current genes in textarea
+  const [currentGeneLists, setGeneLists] = useState([]); //sets the current gene lists in select box
+  const [selectedGeneList, setSelectedGeneList] = useState(); //sets the currently selected gene list in the select box
+  const [newGeneListName, setNewGeneListName] = useState();
 
-
-    function replaceGene(oldSymbol, newSymbol) {
-      console.log("In replace Gene");
-      setGenes(
-      currentGenes.toUpperCase().replace(
-          new RegExp(`\\b${oldSymbol.toUpperCase()}\\b`, 'g'),
-                  () => newSymbol.trim()
-                  .replace(/^\s+|\s+$/g, '')
-                  .replace(/[ \+]+/g, ' ').toUpperCase()
-      ))
-  }
-    let geneListNames = new Set();
-    get("geneListNames").then((val) => {  
-     if(val) geneListNames = val;
-     else geneListNames = new Set(); 
-    })
-
-    useEffect(() => {
-      refreshList();
-      console.log("currentGeneLists", currentGeneLists)
-    },[]);
-
-    const numberOfGenesEntered = currentGenes?.trim()?.replaceAll(/\s+|,|;/g, '\n').replaceAll(/\n+/g, '\n')
-    ?.split('\n')
-    ?.reduce((prev, step) => step?.trim()?.length > 0 ? prev + 1 : prev, 0);
-
-    const saveGeneListNames = ()=> {
-      set("geneListNames",geneListNames)       
-   }
-    
-
-    const refreshList = ()=> {
-      return new Promise((resolve, reject) => {    
-      getAllGenelists()
-      .then(genelists => {   
-        console.log("refreshList",genelists )     
-        setGeneLists([...genelists]); //converts sets to array  
-        resolve()          
-      })
-      .catch(error => {
-        toast({
-          message: { "type":  "Error",
-          "icon": true,
-          "heading": "Genelist Retrival",
-          "content": "Failed to get genelists."},
-          autoClose:2000
-        })
-        reject()
-      })   
-   })}
-
-    // Insert a new genelist into the database
-    const addGenelist = (genelistID, geneList) => {
-      console.log("We are in addgenelist", genelistID, geneList)    
-      if(genelistID && geneList)
-        get("genelist_" + genelistID).then(
-          (val) => { 
-            if(val)
-            { 
-              console.log("We are updating genelist", genelistID, geneList)   
-              set("genelist_" + genelistID,geneList).then(()=> 
-              {
-              toast({
-                  message: { "type":  "Success",
-                  "icon": true,
-                  "heading": "Genelist",
-                  "content": "Genelist updated successfully"},
-                  autoClose:2000
-                }) 
-                refreshList().then(()=> 
-                {
-                setSelectedGeneList(genelistID)
-                setGenes(geneList.genes)
-                })
-              })
-            }
-            else
-            {
-              console.log("We are creating genelist", genelistID, geneList) 
-              set("genelist_" + genelistID, geneList).then(()=>
-              {
-                geneListNames.add(genelistID); 
-                saveGeneListNames()               
-                toast({
-                message: { "type":  "Success",
-                "icon": true,
-                "heading": "Genelist",
-                "content": "Genelist saved successfully"},
-                autoClose:2000
-                })
-                refreshList().then(()=> 
-                {
-                setSelectedGeneList(genelistID)
-                setGenes(geneList.genes)
-                })
-              })
-            }
-          }
+  function replaceGene(oldSymbol, newSymbol) {
+    console.log("In replace Gene");
+    setGenes(
+      currentGenes
+        .toUpperCase()
+        .replace(new RegExp(`\\b${oldSymbol.toUpperCase()}\\b`, "g"), () =>
+          newSymbol
+            .trim()
+            .replace(/^\s+|\s+$/g, "")
+            .replace(/[ \+]+/g, " ")
+            .toUpperCase()
         )
-    }
-    
-  // Get all available genelists from the database
-  const getAllGenelists = () => {
-    return  get("geneListNames")
+    );
+  }
+  let geneListNames = new Set();
+  get("geneListNames").then((val) => {
+    if (val) geneListNames = val;
+    else geneListNames = new Set();
+  });
+
+  useEffect(() => {
+    refreshList();
+    console.log("currentGeneLists", currentGeneLists);
+  }, []);
+
+  const numberOfGenesEntered = currentGenes
+    ?.trim()
+    ?.replaceAll(/\s+|,|;/g, "\n")
+    .replaceAll(/\n+/g, "\n")
+    ?.split("\n")
+    ?.reduce((prev, step) => (step?.trim()?.length > 0 ? prev + 1 : prev), 0);
+
+  const saveGeneListNames = () => {
+    set("geneListNames", geneListNames);
   };
 
+  const refreshList = () => {
+    return new Promise((resolve, reject) => {
+      getAllGenelists()
+        .then((genelists) => {
+          console.log("refreshList", genelists);
+          setGeneLists([...genelists]); //converts sets to array
+          resolve();
+        })
+        .catch((error) => {
+          toast({
+            message: {
+              type: "Error",
+              icon: true,
+              heading: "Genelist Retrival",
+              content: "Failed to get genelists.",
+            },
+            autoClose: 2000,
+          });
+          reject();
+        });
+    });
+  };
 
+  // Insert a new genelist into the database
+  const addGenelist = (genelistID, geneList) => {
+    console.log("We are in addgenelist", genelistID, geneList);
+    if (genelistID && geneList)
+      get("genelist_" + genelistID).then((val) => {
+        if (val) {
+          console.log("We are updating genelist", genelistID, geneList);
+          set("genelist_" + genelistID, geneList).then(() => {
+            toast({
+              message: {
+                type: "Success",
+                icon: true,
+                heading: "Genelist",
+                content: "Genelist updated successfully",
+              },
+              autoClose: 2000,
+            });
+            refreshList().then(() => {
+              setSelectedGeneList(genelistID);
+              setGenes(geneList.genes);
+            });
+          });
+        } else {
+          console.log("We are creating genelist", genelistID, geneList);
+          set("genelist_" + genelistID, geneList).then(() => {
+            geneListNames.add(genelistID);
+            saveGeneListNames();
+            toast({
+              message: {
+                type: "Success",
+                icon: true,
+                heading: "Genelist",
+                content: "Genelist saved successfully",
+              },
+              autoClose: 2000,
+            });
+            refreshList().then(() => {
+              setSelectedGeneList(genelistID);
+              setGenes(geneList.genes);
+            });
+          });
+        }
+      });
+  };
+
+  // Get all available genelists from the database
+  const getAllGenelists = () => {
+    return get("geneListNames");
+  };
 
   // Remove a genelist from the database based on ID
   const removeGenelistById = (id) => {
-    console.log("id", id)
-    del("genelist_" + id).then(()=> {
+    console.log("id", id);
+    del("genelist_" + id).then(() => {
       toast({
-        message: { "type":  "Warning",
-        "icon": true,
-        "heading": "Genelist",
-        "content": "Genelist deleted successfully"},
-        autoClose:2000
-      }) 
-      geneListNames.delete(id); 
+        message: {
+          type: "Warning",
+          icon: true,
+          heading: "Genelist",
+          content: "Genelist deleted successfully",
+        },
+        autoClose: 2000,
+      });
+      geneListNames.delete(id);
       saveGeneListNames();
-      refreshList().then(()=> 
-      {
-      if(currentGeneLists.length>0){            
-        setSelectedGeneList(currentGeneLists[0])         
-        getGenelistById(currentGeneLists[0]).then(result => {
-          console.log(result)
-           setGenes(result.genes); 
-        })
-        .catch(error => {
-          console.error(error);
-        });         
-      }
-      else
-      {
-        setSelectedGeneList([])
-        setGenes("")        
-      }
-      })
-
-  });      
+      refreshList().then(() => {
+        if (currentGeneLists.length > 0) {
+          setSelectedGeneList(currentGeneLists[0]);
+          getGenelistById(currentGeneLists[0])
+            .then((result) => {
+              console.log(result);
+              setGenes(result.genes);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        } else {
+          setSelectedGeneList([]);
+          setGenes("");
+        }
+      });
+    });
   };
 
-
-   // Get a genelist from the database based on ID
-   const getGenelistById = (id) => {
-    return  get("genelist_" + id)
+  // Get a genelist from the database based on ID
+  const getGenelistById = (id) => {
+    return get("genelist_" + id);
   };
-
-
-
 
   useEffect(() => {
     setPerturbationList(currentGenes);
-  },[setPerturbationList,currentGenes]);
-  const dictionary = new Set("AKT1", "AKT2", "AKT3")
+  }, [setPerturbationList, currentGenes]);
+  const dictionary = new Set("AKT1", "AKT2", "AKT3");
 
-
-const props = { 
+  const props = {
     oql: {
-      query: [{ gene: 'AAA', alterations: false }, { gene: 'BBB', alterations: false }],
-  },
-  validatingGenes: false, 
-  replaceGene: replaceGene,
-  wrapTheContent: false,
-  genes: {
-    found: [],
-            suggestions: [
-                {
-                    alias: 'AAA',
-                    genes: [
-                        {
-                            entrezGeneId: 351,
-                            hugoGeneSymbol: 'APP',
-                            type: 'protein-coding',
-                        },
-                    ],
-                },{
-                  alias: 'BBB',
-                  genes: [
-                      {
-                          geneticEntityId: 296,
-                          entrezGeneId: 351,
-                          hugoGeneSymbol: 'APaP',
-                          type: 'protein-coding',
-                      },
-                  ],
-              },
-            ],
-    
-}
-};
-
-
+      query: [
+        { gene: "AAA", alterations: false },
+        { gene: "BBB", alterations: false },
+      ],
+    },
+    validatingGenes: false,
+    replaceGene: replaceGene,
+    wrapTheContent: false,
+    genes: {
+      found: [],
+      suggestions: [
+        {
+          alias: "AAA",
+          genes: [
+            {
+              entrezGeneId: 351,
+              hugoGeneSymbol: "APP",
+              type: "protein-coding",
+            },
+          ],
+        },
+        {
+          alias: "BBB",
+          genes: [
+            {
+              geneticEntityId: 296,
+              entrezGeneId: 351,
+              hugoGeneSymbol: "APaP",
+              type: "protein-coding",
+            },
+          ],
+        },
+      ],
+    },
+  };
 
   const handleInputChange = (event) => {
     /*
@@ -239,84 +251,122 @@ const props = {
   };
 
   return (
-    <div className={styles.mainView}> 
-    <Card heading={<Heading>Genelist</Heading>}>
-     <Row spacing={0} width="100%" height="15%" >  
-     <div className={styles.subItems}>  
-    <Field label='Saved Lists' labelLeft labelWidth="80px" helpText="Your locally saved genelists" className={styles.mainView}>
-        
-        <Select
-          onChange={({ target: { value} }) => {
-            console.log("value",value)
-            setSelectedGeneList(value);                           
-            
-            getGenelistById(value).then(result => {
-              console.log(result)
-               setGenes(result.genes); 
-            })
-            .catch(error => {
-              console.error(error);
-            });   
-          }        
-                    
-          }          
-          options={currentGeneLists}
-          value={selectedGeneList}
-        />
-      </Field>
-      </div>
-    </Row> 
-  
-    <Row spacing={0} width="100%" height="70%">  
-    <div className={styles.subItems}>       
+    <div className={styles.mainView}>
+      <Card heading={<Heading>Genelist</Heading>}>
+        <Row spacing={0} width="100%" height="15%">
+          <div className={styles.subItems}>
+            <Field
+              label="Saved Lists"
+              labelLeft
+              labelWidth="80px"
+              helpText="Your locally saved genelists"
+              className={styles.mainView}
+            >
+              <Select
+                onChange={({ target: { value } }) => {
+                  console.log("value", value);
+                  setSelectedGeneList(value);
 
-           <textarea value={currentGenes} onChange={handleInputChange} rows="10" cols="35" />
-           <Spacer height={30} />
-           <GeneSymbolValidatorMessage {...props} />
-        <Spacer height={3} />
-        <Text>{numberOfGenesEntered}</Text>
-    </div>
-    </Row> 
-   
-   
-    <Row spacing={0} width="100%" height="15%">
-    
-    <div className={styles.subItems}>  
-    <Flex  alignItems="center"
-    justifyContent="space-between"
-    //className={styles.mainView}
-    //  style={{width: "100%"}}    
-    >
- 
-        <Button 
-        colored="danger"
-        disabled = {selectedGeneList === undefined || currentGeneLists.length===0}
-        icon={<FaTrash />}
-        label="DELETE" 
-        onClick={() => {
-          console.log("Deleting",selectedGeneList)          
-          removeGenelistById(selectedGeneList)}
-          }       
-        /> 
+                  getGenelistById(value)
+                    .then((result) => {
+                      console.log(result);
+                      setGenes(result.genes);
+                    })
+                    .catch((error) => {
+                      console.error(error);
+                    });
+                }}
+                options={currentGeneLists}
+                value={selectedGeneList}
+              />
+            </Field>
+          </div>
+        </Row>
 
-        <Popover disabled = {numberOfGenesEntered===0} content={<InputGroup>
-            <Input value={newGeneListName??"New Gene List"} width="150px"  onChange={({ target: { value } }) => setNewGeneListName(value)}/>
-            <Button colored="success" label="Save"  icon={<FaSave />} disabled = {false} 
-            onClick={() =>{ 
-              addGenelist(newGeneListName,{label: newGeneListName,timestamp : Date.now(), value:newGeneListName, genes :currentGenes})
-              //set genelist to current one                     
-            }}/>
-            <Button label="Cancel" icon={<FaWindowClose />}  onClick={function Tl(){}}/></InputGroup>}>
-            <Button colored label="SAVE LIST"  icon={<FaSave />} disabled= {numberOfGenesEntered===0}/>
-        </Popover>  
-        </Flex>
-        </div>
-    </Row> 
-    
-    </Card> 
+        <Row spacing={0} width="100%" height="70%">
+          <div className={styles.subItems}>
+            <textarea
+              value={currentGenes}
+              onChange={handleInputChange}
+              rows="10"
+              cols="35"
+            />
+            <Spacer height={30} />
+            <GeneSymbolValidatorMessage {...props} />
+            <Spacer height={3} />
+            <Text>{numberOfGenesEntered}</Text>
+          </div>
+        </Row>
+
+        <Row spacing={0} width="100%" height="15%">
+          <div className={styles.subItems}>
+            <Flex
+              alignItems="center"
+              justifyContent="space-between"
+              //className={styles.mainView}
+              //  style={{width: "100%"}}
+            >
+              <Button
+                colored="danger"
+                disabled={
+                  selectedGeneList === undefined ||
+                  currentGeneLists.length === 0
+                }
+                icon={<FaTrash />}
+                label="DELETE"
+                onClick={() => {
+                  console.log("Deleting", selectedGeneList);
+                  removeGenelistById(selectedGeneList);
+                }}
+              />
+
+              <Popover
+                disabled={numberOfGenesEntered === 0}
+                content={
+                  <InputGroup>
+                    <Input
+                      value={newGeneListName ?? "New Gene List"}
+                      width="150px"
+                      onChange={({ target: { value } }) =>
+                        setNewGeneListName(value)
+                      }
+                    />
+                    <Button
+                      colored="success"
+                      label="Save"
+                      icon={<FaSave />}
+                      disabled={false}
+                      onClick={() => {
+                        addGenelist(newGeneListName, {
+                          label: newGeneListName,
+                          timestamp: Date.now(),
+                          value: newGeneListName,
+                          genes: currentGenes,
+                        });
+                        //set genelist to current one
+                      }}
+                    />
+                    <Button
+                      label="Cancel"
+                      icon={<FaWindowClose />}
+                      onClick={function Tl() {}}
+                    />
+                  </InputGroup>
+                }
+              >
+                <Button
+                  colored
+                  label="SAVE LIST"
+                  icon={<FaSave />}
+                  disabled={numberOfGenesEntered === 0}
+                />
+              </Popover>
+            </Flex>
+          </div>
+        </Row>
+      </Card>
     </div>
   );
 };
-
 
 export default Genelist;
